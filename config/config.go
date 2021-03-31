647379f9c448ddb3ac517d0a0d22012ed5e3fd73
Edgar Albalate <dev.gardo@gmail.com>
2021-03-31 12:10:36 +0200

    Config loading should priorize executable location files.

--- config/config.go
+++ config/config.go
@@ -1,10 +1,24 @@
 package config
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 
 	"gopkg.in/yaml.v3"
 )
+
+var ConfigPath string = "config.yaml"
+
+func init() {
+	exe, err := os.Executable()
+	if err == nil {
+		ConfigPath = path.Join(path.Dir(exe), "config.yaml")
+	} else {
+		fmt.Println("Can't get executable folder")
+	}
+}
 
 type Config struct {
 	Name  string `yaml:"name"`
@@ -19,7 +33,7 @@ func parseYaml(data []byte) Config {
 }
 
 func Read() (Config, error) {
-	data, err := ioutil.ReadFile("config.yaml")
+	data, err := ioutil.ReadFile(ConfigPath)
 	if err == nil {
 		return parseYaml(data), nil
 	}
