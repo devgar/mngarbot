cbbac9a830b1f30ea9e6b569bd9d1f38f9333328
Edgar Albalate <dev.gardo@gmail.com>
2021-03-31 19:35:49 +0200

    Config building WIP

--- config/config.go
+++ config/config.go
@@ -5,19 +5,28 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"strconv"
 
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/shibukawa/configdir"
 	"gopkg.in/yaml.v3"
 )
 
-var ConfigPath string = "config.yaml"
+var (
+	ConfigPath string = "config.yaml"
+	config     Config = Config{}
+	adminErr   error
+	pkgName    string = "mngarbot"
+)
 
-func init() {
-	exe, err := os.Executable()
-	if err == nil {
-		ConfigPath = path.Join(path.Dir(exe), "config.yaml")
-	} else {
-		fmt.Println("Can't get executable folder")
+func buildRoutes() []string {
+	configdir.New("devgar", pkgName)
+	ways := []string{}
+	if cfgDir, err := os.UserConfigDir(); err != nil {
+		ways = append(ways, path.Join(cfgDir, pkgName, "config.yaml"))
 	}
+	ways = append(ways)
+	return ways
 }
 
 type Config struct {
@@ -32,10 +41,36 @@ func parseYaml(data []byte) Config {
 	return c
 }
 
-func Read() (Config, error) {
+func read() (Config, error) {
 	data, err := ioutil.ReadFile(ConfigPath)
 	if err == nil {
 		return parseYaml(data), nil
 	}
 	return Config{}, err
+}
+
+func Get() Config {
+	return config
+}
+
+func init() {
+	TOKEN := os.Getenv("TOKEN")
+	ADMIN, _ := strconv.Atoi(os.Getenv("ADMIN"))
+	exe, err := os.Executable()
+	dir := path.Dir(exe)
+	name := path.Base(exe)
+	fmt.Println("NAME: ", name)
+	os.Exit(3)
+	if err == nil {
+		ConfigPath = path.Join(dir, "config.yaml")
+	} else {
+		fmt.Println("Can't get executable folder")
+	}
+	config, _ := read()
+	if config.Token == "" {
+		config.Token = TOKEN
+	}
+	if config.ID == 0 {
+		config.ID = ADMIN
+	}
 }
