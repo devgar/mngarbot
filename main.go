ddf4bd954f6578a536b74c4010fb2811ebd99e1d
Edgar Albalate <dev.gardo@gmail.com>
2021-04-08 09:55:20 +0200

    Changed logging Added -c [CHAT_ID] option for logging message

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,10 +12,46 @@ import (
 )
 
 var (
+	chat    int64  = 0
 	message string = ""
 )
 
+func Echo(m tb.Message) tb.MessageConfig {
+	msg := tb.NewMessage(m.Chat.ID, m.Text)
+	msg.ReplyToMessageID = m.MessageID
+	return msg
+}
+
+func logMsgData(m tb.Message) {
+	mFromID := ""
+	mFromName := ""
+	mType := "Chat"
+	if m.From != nil {
+		mFromID = fmt.Sprintf("%d", m.From.ID)
+		mFromName = m.From.UserName
+	}
+	if m.Chat.IsChannel() {
+		mType = "Channel"
+	}
+	if m.Chat.IsGroup() {
+		mType = "Group"
+	}
+	if m.Chat.IsSuperGroup() {
+		mType = "SuperGroup"
+	}
+	log.Printf(
+		"(%s)\n#%s [%s]\n#%d [%s]\n  %s",
+		mType,
+		mFromID,
+		mFromName,
+		m.Chat.ID,
+		m.Chat.Title,
+		m.Text,
+	)
+}
+
 func init() {
+	flag.Int64Var(&chat, "c", 0, "Chat to send a message")
 	flag.StringVar(&message, "m", "", "Message to send to admin")
 	flag.Parse()
 }
@@ -28,7 +65,10 @@ func main() {
 	}
 
 	if message != "" {
-		msg := tb.NewMessage(c.ID, message)
+		if chat == 0 {
+			chat = c.ID
+		}
+		msg := tb.NewMessage(chat, message)
 		b.Send(msg)
 		os.Exit(0)
 	}
@@ -44,27 +84,26 @@ func main() {
 	updates, err := b.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {
-			log.Printf("Ignored empty message")
+		if update.Message != nil {
+			if c.ID == 0 {
+				log.Printf("[No ADMIN set] %d %s", update.Message.From.ID, update.Message.From.UserName)
+				continue
+			}
+
+			if update.Message.Chat.IsPrivate() && c.ID != int64(update.Message.From.ID) {
+				msg := tb.NewMessage(update.Message.Chat.ID, "I'm not allowed to talk to you")
+				b.Send(msg)
+			}
+
+			logMsgData(*update.Message)
+		} else if update.ChannelPost != nil {
+			logMsgData(*update.ChannelPost)
+		} else if update.EditedMessage != nil {
+			log.Printf("Edited message:\n  %s", update.EditedMessage.Text)
+		} else if update.EditedChannelPost != nil {
+			log.Printf("Edited message:\n  %s", update.EditedChannelPost.Text)
+		} else {
+			log.Printf("Unhandled update type")
 		}
-
-		if c.ID == 0 {
-			log.Printf("[Not admin from] %d %s", update.Message.From.ID, update.Message.From.UserName)
-			continue
-		}
-
-		if c.ID != int64(update.Message.From.ID) {
-			msg := tb.NewMessage(update.Message.Chat.ID, "I'm not allowed to talk to you")
-			b.Send(msg)
-		}
-
-		// update
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tb.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		b.Send(msg)
 	}
 }
